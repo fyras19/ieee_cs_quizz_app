c7f576385adb9a31ff5086dc0bfc9e8af3118a89
LENOVO <bouazizyessine08@gmail.com>
2021-02-20 20:38:42 +0100

    Four Commit

--- lib/widgets/ansewer.dart
+++ lib/widgets/ansewer.dart
@@ -9,16 +9,15 @@ class Ansewer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+
       children:<Widget>[
+
         Container(
-
+          
         width: 250,
-
         child: RaisedButton(
-
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(80.0),
-
           ),
           color: Colors.blue,
           textColor: Colors.white,
